50e814252144de735801c2a249a70bb1c1b93b5e
aafowler <afowler68127@gmail.com>
2024-12-15 01:55:31 -0600

    commit message

---
+++ nbagui.py
@@ -0,0 +1,87 @@
+import tkinter as tk
+from tkinter import ttk
+from nbalogic import get_data, get_players, get_stats
+import pandas as pd
+
+class NBAStatsGUI:
+    """
+        Initializes the NBAStatsGUI with necessary components.
+
+        Args:
+            master (tk.Tk): Is the window of the app.
+    """
+    def __init__(self, master):
+        self.master = master
+        self.df = get_data()
+        self.team_players = get_players(self.df)
+        self.team_players_sorted = sorted(self.team_players.keys())
+
+        self.header_title = ttk.Label(master, text='NBA STATS', font=('Arial', 16))
+        self.header_title.grid(row=0, column=0, columnspan=3, pady=10, padx=45, sticky='nw')
+
+        self.team_abv = tk.StringVar(master)
+        self.player_tag = tk.StringVar(master)
+
+        self.team_name = ttk.Label(master, text='Team:')
+        self.team_name.grid(row=1, column=0, padx=5, pady=5, sticky='w')
+
+        self.team_dropdown = ttk.Combobox(master, textvariable=self.team_abv, values=self.team_players_sorted)
+        self.team_dropdown.grid(row=1, column=1, padx=5, pady=5, sticky='w')
+        self.team_dropdown.bind('<<ComboboxSelected>>', self.update_player_dropdown)
+
+        self.player_name = ttk.Label(master, text='Player:')
+        self.player_name.grid(row=2, column=0, padx=5, pady=5, sticky='w')
+
+        self.player_dropdown = ttk.Combobox(master, textvariable=self.player_tag)
+        self.player_dropdown.grid(row=2, column=1, padx=5, pady=5, sticky='w')
+        self.player_dropdown.bind('<<ComboboxSelected>>', self.display_stats)
+
+        self.points_label = ttk.Label(master, text='PTS:')
+        self.points_label.grid(row=3, column=1, padx=5, pady=5, sticky='w')
+
+        self.rebounds_label = ttk.Label(master, text='TRB:')
+        self.rebounds_label.grid(row=4, column=1, padx=5, pady=5, sticky='w')
+
+        self.assists_label = ttk.Label(master, text='AST:')
+        self.assists_label.grid(row=5, column=1, padx=5, pady=5, sticky='w')
+
+        self.reset_button = ttk.Button(master, text='Reset', command=self.reset_gui)
+        self.reset_button.grid(row=6, column=0, columnspan=2, pady=10)
+
+    def update_player_dropdown(self, *args):
+        """
+        Updates dropdown to the selected team.
+        """
+        selected_team = self.team_abv.get()
+        self.player_dropdown['values'] = self.team_players.get(selected_team, [])
+        self.player_tag.set('')
+        self.points_label.config(text='PTS:')
+        self.rebounds_label.config(text='TRB:')
+        self.assists_label.config(text='AST:')
+
+    def display_stats(self, *args):
+        """
+        Updates stats variable labels.
+        """
+        selected_team = self.team_abv.get()
+        selected_player = self.player_tag.get()
+        stats = get_stats(self.df, selected_team, selected_player)
+        if stats:
+            self.points_label.config(text=f'PTS: {stats["PTS"]}')
+            self.rebounds_label.config(text=f'TRB: {stats["TRB"]}')
+            self.assists_label.config(text=f'AST: {stats["AST"]}')
+        else:
+            self.points_label.config(text='PTS: Not Found')
+            self.rebounds_label.config(text='TRB: Not Found')
+            self.assists_label.config(text='AST: Not Found')
+
+    def reset_gui(self):
+        """
+        Clears GUI
+        """
+        self.team_abv.set('')
+        self.player_tag.set('')
+        self.player_dropdown['values'] = []
+        self.points_label.config(text='PTS:')
+        self.rebounds_label.config(text='TRB:')
+        self.assists_label.config(text='AST:')
---
+++ nbalogic.py
@@ -0,0 +1,59 @@
+import pandas as pd
+
+def get_data():
+    """
+
+    Retrievesplayer stats from basketball-reference.com for the 2025 season,
+    converts the data into a Pandas DataFrame, and saves it as a CSV file named 'nba_2025_stats.csv'.
+
+    Returns:
+        pandas.DataFrame: DataFrame that has all NBA player stats.
+    """
+    stats = pd.read_html('https://www.basketball-reference.com/leagues/NBA_2025_per_game.html')
+    df = stats[0]
+    df.to_csv('nba_2025_stats.csv', index=False)
+    return df
+
+def get_players(df):
+    """
+    Creates a dictionary With teams as the key and players as the value.
+
+    Handles missing team or player data.
+
+    Args:
+        df: DataFrame containing NBA player data.
+
+    Returns:
+        team_players: A dictionary connecting team names to lists of players on the team.
+    """
+    team_players = {}
+    for index, row in df.iterrows():
+        team = row['Team']
+        player = row['Player']
+        if pd.notna(team) and pd.notna(player):
+            if team not in team_players:
+                team_players[team] = []
+            team_players[team].append(player)
+    return team_players
+
+def get_stats(df, team, player):
+    """
+    Gets the points, rebounds, and assists of selected player.
+
+    Args:
+        df: DataFrame containing NBA player stats.
+        team (str): The name of the team.
+        player (str): The name of the player.
+
+    Returns:
+        dict or None: A dictionary with players points(pts), rebounds(RBS), and assists (AST),
+        or no stats.
+    """
+    player_data = df[(df['Team'] == team) & (df['Player'] == player)]
+    if not player_data.empty:
+        return {
+            "PTS": player_data['PTS'].iloc[0],
+            "TRB": player_data['TRB'].iloc[0],
+            "AST": player_data['AST'].iloc[0]
+        }
+    return None
